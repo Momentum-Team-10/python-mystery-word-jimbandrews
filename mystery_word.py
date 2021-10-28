9e7c47edbabb67d78557698b9b39e417506bbc1f
James Andrews <jbandrew14@gmail.com>
2021-10-28 14:51:32 -0400

    changed sort_by_difficulty to use list comprehension, consolidated setup code into its own function, started working on displaying info for the round

--- mystery_word.py
+++ mystery_word.py
@@ -1,27 +1,51 @@
-
+import random
 
 with open("words.txt") as file:
     word_string = file.read()
     word_list = word_string.split("\n")
 
-ycw_easy = []
-easy = []
-normal = []
-hard = []
-
 
 def sort_by_difficulty(x=word_list):
-    for word in x:
-        if len(word) < 4:
-            ycw_easy.append(word)
-        elif 3 < len(word) < 7:
-            easy.append(word)
-        elif 6 < len(word) < 9:
-            normal.append(word)
-        elif 8 < len(word):
-            hard.append(word)
+    ycw_easy = [word for word in word_list if len(word) < 4]
+    easy = [word for word in word_list if 3 < len(word) < 7]
+    normal = [word for word in word_list if 6 < len(word) < 9]
+    hard = [word for word in word_list if 8 < len(word)]
+    return ycw_easy, easy, normal, hard
 
 
-sort_by_difficulty()
+def pick_a_word(difficulty):
+    return difficulty[random.randint(0, len(difficulty)-1)]
 
-print(len(ycw_easy), len(easy), len(normal), len(hard))
+
+def game_setup():
+    difficulty = input(
+        "Please select your difficulty (ycw_easy, easy, normal, or hard): "
+        )
+    ycw_easy, easy, normal, hard = sort_by_difficulty()
+    picked_word = pick_a_word(difficulty)
+    return picked_word
+
+
+def display_word(word, letters):
+    guessed_list = []
+    for letter in word:
+        if letter in letters:
+            guessed_list.append(letter)
+        else:
+            guessed_list.append('_')
+    print(' '.join(guessed_list))
+
+
+
+def run_game():
+    mystery_word = "lamb"
+    guesses_left = 8
+    incorrect_guesses = []
+    correct_guesses = []
+    display_word(mystery_word, correct_guesses)
+    if len(incorrect_guesses) > 0:
+        print(f"Letter Graveyard: {', '.join(incorrect_guesses)}")
+    print(f"You have {guesses_left} guesses left.")
+
+
+run_game()
