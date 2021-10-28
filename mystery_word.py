e5c90af668124ea017bf9778bfd0b390df834e3b
James Andrews <jbandrew14@gmail.com>
2021-10-28 19:12:34 -0400

    finished if/else tree going through valid and invalid guesses

--- mystery_word.py
+++ mystery_word.py
@@ -18,6 +18,7 @@ def pick_a_word(difficulty):
 
 
 def game_setup():
+    print("Welcome to the Mystery Word Game!")
     difficulty = input(
         "Please select your difficulty (ycw_easy, easy, normal, or hard): "
         )
@@ -26,7 +27,7 @@ def game_setup():
     return picked_word
 
 
-def display_word(word, letters):
+def display_word(word, letters=[]):
     guessed_list = []
     for letter in word:
         if letter in letters:
@@ -36,16 +37,44 @@ def display_word(word, letters):
     print(' '.join(guessed_list))
 
 
+def start_game(mystery_word, correct):
+    print("The Mystery Word has been selected!")
+
+
 
 def run_game():
     mystery_word = "lamb"
     guesses_left = 8
     incorrect_guesses = []
     correct_guesses = []
-    display_word(mystery_word, correct_guesses)
-    if len(incorrect_guesses) > 0:
-        print(f"Letter Graveyard: {', '.join(incorrect_guesses)}")
-    print(f"You have {guesses_left} guesses left.")
+    game_completed = False
+    print("Welcome to the Mystery Word Game!")
+
+    while game_completed == False:
+        display_word(mystery_word, correct_guesses)
+        if len(incorrect_guesses) > 0:
+            print(f"Letter Graveyard: {', '.join(incorrect_guesses)}")
+        print(f"You have {guesses_left} guesses left.")
+        user_guess = input("Please guess a letter: ")
+        if len(user_guess) > 1:
+            print("Please guess one letter at a time.")
+        elif user_guess == " ":
+            print("Please guess a letter of the alphabet.")
+        elif user_guess == "":
+            print("Please guess a letter of the alphabet.")
+        elif user_guess in incorrect_guesses or correct_guesses:
+            print("You have already guessed that letter. Please try again.")
+        else:
+            if user_guess not in mystery_word:
+                print("Sorry, that guess was incorrect.")
+                incorrect_guesses.append(user_guess)
+                guesses_left -= 1
+            else:
+                print("That's correct!")
+                correct_guesses.append(user_guess)
+        
+
+
 
 
 run_game()
