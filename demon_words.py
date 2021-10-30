73b54e802abff3f0687c21684de6d2139c3ee537
James Andrews <jbandrew14@gmail.com>
2021-10-30 11:05:51 -0400

    fixed building word families, at least for first letter

--- demon_words.py
+++ demon_words.py
@@ -45,39 +45,55 @@ def create_perm_seeds(letter, blanks):
     return perm_seeds
 
 
+def set_passing_match_score(permutation):
+    passing_score = 0
+    for letter in permutation:
+        if letter == '_':
+            continue
+        else:
+            passing_score += 1
+    return passing_score
+
+
 def find_new_mystery_list(
     guessed_letter, current_word, main_list, perm_dict
 ):
+    matched_words = []
     current_blanks = current_word.copy()
     for letter in current_word:
         if letter != "_":
             current_blanks.remove(letter)
-    all_permutations = {tuple(current_blanks): []}
+    all_permutations = {}
     permutation_seeds = create_perm_seeds(guessed_letter, current_word)
     for seed in permutation_seeds:
         perms = build_permutations(seed)
         for permutation in perms:
             all_permutations[tuple(permutation)] = []
+    all_permutations[tuple(current_blanks)] = []
     for permutation in all_permutations.keys():
         perm_list = list(permutation)
+        # ignoring these lines for now, focus on getting first guess to work
         for letter in perm_dict.keys():
             perm_list.insert(perm_dict[letter], letter)
         perm_string = "".join(perm_list).lower()
+        passing_score = set_passing_match_score(perm_string)
         for word in main_list:
+            if word in matched_words:
+                continue
             match_score = 0
-            for index in range(len(perm_string)):
-                if perm_string[index] == "_":
+            for index in range(len(word)):
+                if word[index] == perm_string[index]:
                     match_score += 1
-                    continue
-                elif perm_string[index] == word[index]:
-                    match_score += 1
-            if match_score == len(perm_string):
+            if match_score == passing_score:
                 all_permutations[permutation].append(word)
-    breakpoint()
-    # everythiing below this line in function is temporary placeholders to
-    # get code to run for debugging
-    new_mystery_list = []
-    new_current_word = []
+                matched_words.append(word)
+    all_perm_keys = list(all_permutations.keys())
+    max_word_family = all_perm_keys[0]
+    for permutation in all_perm_keys:
+        if len(all_permutations[permutation]) > len(all_permutations[max_word_family]):
+            max_word_family = permutation
+    new_mystery_list = all_permutations[max_word_family]
+    new_current_word = max_word_family
     return new_mystery_list, new_current_word
 
 
@@ -97,9 +113,10 @@ def run_game(word_list):
             user_guess = input(
                 "Please guess a letter from the alphabet: "
             ).upper()
-            mystery_list, guessed_word = find_new_mystery_list(
+            mystery_list, guessed_word_tuple = find_new_mystery_list(
                 user_guess, guessed_word, mystery_list, permanent_letters
             )
+            guessed_word = list(guessed_word_tuple)
 
 
 run_game(main_word_list)
