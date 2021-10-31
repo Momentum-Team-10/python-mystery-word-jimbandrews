bfe8fbdaab20355bf8f5d223cc3f337feaa2d4a0
James Andrews <jbandrew14@gmail.com>
2021-10-31 10:11:06 -0400

    fixed bug with filled families dict that was affecting fill_families_dict and find_new_list_and_word f'ns; added game_display f'n; added choose_difficulty f'n; moved file opening to inside run_game; implemented choosing difficulty and building mystery_list to run_game

--- new_demon_words.py
+++ new_demon_words.py
@@ -1,13 +1,8 @@
 from itertools import combinations
 
 
-with open("words.txt") as file:
-    word_string = file.read()
-    main_word_list = word_string.split("\n")
-
-
 def build_word_list(length, word_list):
-    return [word.lower() for word in word_list if len(word) == length]
+    return [word.upper() for word in word_list if len(word) == length]
 
 
 def build_permutations(indices):
@@ -58,7 +53,8 @@ def convert_word(blanked, word):
     return ''.join(listed_word)
 
 
-def fill_families_dict(families_dict, words_list):
+# need to fix a bug here; lines 74,75 only need to run if it is the first guess
+def fill_families_dict(families_dict, words_list, guessed_word):
     matched_words = []
     list_copy = words_list.copy()
     for permutation in families_dict:
@@ -70,8 +66,7 @@ def fill_families_dict(families_dict, words_list):
                 families_dict[permutation].append(word)
                 matched_words.append(word)
                 list_copy.remove(word)
-    empty_string = len(words_list[0]) * '_'
-    families_dict[empty_string] = list_copy
+    families_dict[''.join(guessed_word)] = list_copy
     return families_dict
 
 
@@ -81,14 +76,31 @@ def find_new_list_and_word(current_word, new_guess, words_list):
     families_dict = build_families_dict(
         index_permutations, current_word, new_guess
     )
-    filled_families = fill_families_dict(families_dict, words_list)
-    max_list_key = len(current_word) * '_'
+    filled_families = fill_families_dict(families_dict, words_list, current_word)
+    max_list_key = ''.join(current_word)
     for permutation in filled_families:
         if len(filled_families[permutation]) > len(filled_families[max_list_key]):
             max_list_key = permutation
-    new_current_word = max_list_key
+    new_current_word = list(max_list_key)
     new_word_list = filled_families[max_list_key]
     return new_current_word, new_word_list
+
+
+def game_display(guessed_word, guessed_letters):
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print(" ".join(guessed_word))
+    if len(guessed_letters) > 0:
+        print(f"Letter Graveyard: {', '.join(guessed_letters)}")
+
+
+def choose_difficulty():
+    word_length = input("How long would you like the mystery word to be? ")
+    while word_length.isnumeric() is False:
+        print("Sorry, that is not a valid number.")
+        word_length = input("Please enter a positive whole number: ")
+    return int(word_length)
 
 
 # add in word_list as a parameter later
@@ -97,14 +109,25 @@ def run_game():
 
     while new_game is True:
         # placeholders to get code to work
-        mystery_list = ['echo', 'heal', 'best', 'lazy']
-        guessed_word = ['_', '_', '_', '_']
+        # mystery_list = ['echo', 'heal', 'best', 'lazy']
+        # guessed_word = ['_', '_', '_', '_']
+
+        with open("words.txt") as file:
+            word_string = file.read()
+            main_word_list = word_string.split("\n")
+
+        mystery_length = choose_difficulty()
+        guessed_word = mystery_length * '_'
+        mystery_list = build_word_list(mystery_length, main_word_list)
+        breakpoint()
         game_completed = False
         guessed_letters = []
-        
+
         while game_completed is False:
-            print(" ".join(guessed_word))
+            game_display(guessed_word, guessed_letters)
             user_guess = input("Please guess a letter from the alphabet: ")
+            while len(user_guess) != 1:
+                user_guess = input("Please guess one letter at a time: ")
             while user_guess.isalpha() is False:
                 user_guess = input("That is not a letter. Please try again: ")
             user_guess = user_guess.upper()
